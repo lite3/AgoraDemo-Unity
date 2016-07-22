53f81535f9e9fba8817667fb5f4442f112688478
lite3 <lite3@qq.com>
2016-07-22 11:54:41 +0800

    script build_plugin support copy so to Unity

--- Native/build_plugin.bat
+++ Native/build_plugin.bat
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 
 echo Cleaning up / removing build folders...
 rmdir /S /Q libs
@@ -7,6 +7,9 @@ echo Compiling NativeCode.c...
 call "%NDK_ROOT%/ndk-build" -C .  %*
 rem $NDK_ROOT/ndk-build -C "$DIR/src" NDK_MODULE_PATH=src NDK_APPLICATION_MK=$DIR/Application.mk $*
 rem mv libs/armeabi/libnative.so ..
+
+echo Copy .so to Unity folder
+xcopy /Y  "libs\armeabi-v7a\libnative.so" "..\Assets\Plugins\Android\Agora\libs\armeabi-v7a\libnative.so"
 
 echo Done!
 pause
--- Native/build_plugin.sh
+++ Native/build_plugin.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 echo ""
-echo "Compiling NativeCode.c..."
+echo "Cleaning up / removing build folders..."  #optional..
 rm -rf libs
 rm -rf obj
+echo "Compiling NativeCode.c..."
 $NDK_ROOT/ndk-build -C .  $*
 #$NDK_ROOT/ndk-build -C "$DIR/src" NDK_MODULE_PATH=src NDK_APPLICATION_MK=$DIR/Application.mk $*
 #mv libs/armeabi/libnative.so ..
 
 echo ""
-echo "Cleaning up / removing build folders..."  #optional..
 
+echo "Copy .so to Unity folder"
+cp -f libs/armeabi-v7a/libnative.so ../Assets/Plugins/Android/Agora/libs/armeabi-v7a/libnative.so
 
 echo ""
 echo "Done!"
-pause
