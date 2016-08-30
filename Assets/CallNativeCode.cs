a6cd2fe2657c14d842f88c88e8e89cdef29469e0
lite3 <lite3@qq.com>
2016-08-30 20:26:21 +0800

    disable call native code on iOS

--- Assets/CallNativeCode.cs
+++ Assets/CallNativeCode.cs
@@ -4,6 +4,7 @@ using System.Runtime.InteropServices;
 
 public class CallNativeCode {
 
+#if UNITY_ANDROID 
 	[DllImport("native")]
 	private static extern float add(float x, float y);
 
@@ -14,19 +15,24 @@ public class CallNativeCode {
     private static extern void unisdkAgoraSetData(short[] data, int length, int fs, int channels, int volume);
 
     public static bool isOk = false;
+#endif
 
 	public static void Init()
     {
+#if UNITY_ANDROID
         int result = unisdkAgoraInit();
         Debug.LogError("unisdkAgoraInit: " + result);
         isOk = result == 0;
+#endif
     }
 
     public static void SetData(short[] data, int length, int fs, int channels, int volume)
     {
+#if UNITY_ANDROID
         if (isOk)
         {
             unisdkAgoraSetData(data, length, fs, channels, volume);
         }
+#endif
     }
 }
